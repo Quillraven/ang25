82a46acc3bbcc1c00da54bd840435ffd31d9ed65
Quillraven <=>
2025-04-23 16:33:38 +0200

    update navigation bar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,6 +7,26 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit {
+
+  @ViewChild('navMenu') navMenuElement!: ElementRef<HTMLDetailsElement>;
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.navMenuElement) {
+      console.error("Couldn't find the 'navMenu' element.");
+    }
+  }
+
+  closeMenu(): void {
+    if (!this.navMenuElement || !this.navMenuElement.nativeElement) {
+      console.warn("Cannot close menu because of missing 'navMenuElement'");
+      return;
+    }
+
+    this.renderer.removeAttribute(this.navMenuElement.nativeElement, 'open');
+  }
 
 }
